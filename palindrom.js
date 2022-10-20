4285abf5383071222fbf3c8b679ddd9038da68a7
khushboo787 <115460837+khushboo787@users.noreply.github.com>
2022-10-20 06:49:54 +0530

    Update palindrom.js

--- palindrom.js
+++ palindrom.js
@@ -2,5 +2,13 @@ let str=racecar;
 let rev_str="";
     for (let i = str.length-1; i>=0; i--) {
            rev_str=rev_str+str[i];
-      }     
-      
+      }  
+  if(str==rev_str)
+  {
+      console.log("Palindrome";
+  }
+    
+  else
+  {
+      console.log("Not a palindrome");
+  }
